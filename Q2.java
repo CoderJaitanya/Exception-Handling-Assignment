3ba1e599ad218231efb075c3940f7f23a20c8889
JAITANYA HANDA <99310839+CoderJaitanya@users.noreply.github.com>
2025-02-03 11:08:05 +0530

    Add files via upload

---
+++ Q2.java
@@ -0,0 +1,25 @@
+class CustomException extends Exception {
+    public CustomException(String message) {
+        super(message);
+    }
+}
+
+public class Q2 {
+    public static void main(String[] args) {
+        try {
+            checkValue(-5);
+        } catch (CustomException e) {
+            System.out.println("Caught CustomException: " + e.getMessage());
+        } finally {
+            System.out.println("Finally block executed after CustomException handling.");
+        }
+    }
+
+    public static void checkValue(int value) throws CustomException {
+        if (value < 0) {
+            throw new CustomException("Value cannot be negative: " + value);
+        } else {
+            System.out.println("Valid value: " + value);
+        }
+    }
+}
